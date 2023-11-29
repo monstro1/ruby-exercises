d7f76269abd8e61491d0cdc9e7e78eb863ca34f4
monstro1 <michael@monstro1.com>
2023-11-29 14:00:56 -0500

    Write remove_last_element method

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -23,9 +23,7 @@ def add_element(array)
 end
 
 def remove_last_element(array)
-  # Step 1: remove the last element from the array
-
-  # Step 2: return the array (because Step 1 returns the value of the element removed)
+  array.pop
   array
 end
 
